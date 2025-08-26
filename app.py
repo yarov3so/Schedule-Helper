6d3a272e148429a659c09bffe2629c8c797f806b
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-08-26 10:34:51 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import re
+import statistics as stat
+import streamlit as st
+
+st.set_page_config(
+    page_title="Mayer Line Calculator",
+    page_icon="MHT.png", 
+)
