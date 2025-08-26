b9e0877c94082bde0fff8b5ab2b70b4dcbdc592d
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-08-26 14:15:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ df["Length (minutes)"]=df["Length (minutes)"].apply(try_int)
 
 for idx in df[["Start","Length (minutes)","End"]].index:
     if df[["Start","Length (minutes)","End"]].loc[idx].isnull().all():
-        st.warning("Please ensure that you have correctly specified at least one of the following for each period (row): Start, End.)
+        st.warning("Please ensure that you have correctly specified at least one of the following for each period (row): Start, End.")
         st.stop()
 
 st.dataframe(df)
