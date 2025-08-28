6a5e82c841984e5dc9502fba64c0bbdfa7148ea7
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-08-27 21:06:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -383,8 +383,8 @@ for idx in df[["Start","Length (minutes)","End"]].index: #df[["Start","Length (m
         st.warning("Please ensure that you have correctly specified at least one of the following for each period (row): Start, End.")
         st.stop()
 
-for idx in df[["Type"]].index:
-    if df[["Type"]].loc[idx].isnull().all():
+for el in df["Type"]:
+    if el not in reqs.keys():
         st.warning("Please remember to specify the type of each period listed above.")
         st.stop()
 
