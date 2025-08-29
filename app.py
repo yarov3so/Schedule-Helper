6de7c8314bed9f5207733de5e4af83fb40259be0
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-08-28 20:57:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -565,9 +565,9 @@ for _, row in df_copy[df_copy["Type"] == "overlap"].iterrows():
     ax.axvline(row["End"], color="red", linestyle="--", linewidth=1.5)
 
 
-    ax.text(row["Start"], -0.5, row["Start"].strftime("%H:%M"),
+    ax.text(row["Start"], -0.7, row["Start"].strftime("%H:%M"),
             rotation=90, va="bottom", ha="center", color="red", fontsize=8)
-    ax.text(row["End"], -0.5, row["End"].strftime("%H:%M"),
+    ax.text(row["End"], -0.7, row["End"].strftime("%H:%M"),
             rotation=90, va="bottom", ha="center", color="red", fontsize=8)
 
 
