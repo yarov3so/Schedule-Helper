84eb228262fc131ce6339127ef896bff1020a78d
Yaro <61938366+yarov3so@users.noreply.github.com>
2026-04-15 17:35:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -493,8 +493,7 @@ df["End"]=df["End"].apply(comprehend)
 
 
 for idx in df[["Start","Length (minutes)","End"]].index: #df[["Start","Length (minutes)","End"]].loc[idx].isnull().all() or (
-    st.markdown(df[["Start","Length (minutes)","End"]].loc[idx])
-    if df[["Start","Length (minutes)","End"]].loc[idx][0]==None and df[["Start","Length (minutes)","End"]].loc[idx][2]==None :
+    if df[["Start","Length (minutes)","End"]].loc[idx]["Start"]==None and df[["Start","Length (minutes)","End"]].loc[idx]["End"]==None :
         st.warning("Please ensure that you have correctly specified at least one of the following for each period (row): Start, End.")
         st.stop()
 
